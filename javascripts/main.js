ec0caf04e6841c84495462acb7185f0a10cc4926
Arvey Jimenez <arve505@gmail.com>
2014-06-01 02:14:39 +0800

    Site updated at 2014-05-31 18:14:39 UTC

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,26 @@
- if($(window).width() > 768){
-        $(".home-page-content").css("height",$(window).height());
+$( document ).ready(function() { 
+
+  if($(window).width() > 768){
+    $(".home-page-content").css("height",$(window).height());
+  }else{
+    $(".home-page-content").css("height","100%");
+  }
+
+  $(window).resize(function(){
+    if($(window).width() > 768){
+      $(".home-page-content").css("height",$(window).height());
     }else{
-        $(".home-page-content").css("height","100%");
+      $(".home-page-content").css("height","100%");
     }
-    $(window).resize(function(){
-        if($(window).width() > 768){
-            $(".home-page-content").css("height",$(window).height());
-        }else{
-            $(".home-page-content").css("height","100%");
-        }
-    });
+  });
+
+  $(".slide-menu").click(function(){
+
+    var body = $('body');
+    body.toggleClass('animate-left');
+    $(".left-sidebar-container").toggleClass('side-menu-animate-left');
+    $(".top-icon").toggleClass('animate-left');
+    return false;
+  });
+
+});
